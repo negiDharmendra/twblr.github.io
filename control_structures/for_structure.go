371f80826a1f2810e6890260024f432fd069a370
Dharmendra Singh <dharmenn@thoughtworks.com>
2016-07-22 16:40:54 +0530

    refactoring for_structure.go

--- control_structures/for_structure.go
+++ control_structures/for_structure.go
@@ -2,14 +2,13 @@ package control_structures
 
 func collatzChainLength(n int) int {
 	i := 0
-	for {
-		if n == 1 {
-			return i
-		} else if n%2 == 0 {
+	for n!=1 {
+		if n&1 {
 			n = n / 2
 		} else {
 			n = (3 * n) + 1
 		}
 		i++
 	}
+	return  i
 }
