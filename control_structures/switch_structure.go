ace8ec0f211bc9c918800aa12b26d32856571676
Dharmendra Singh <dharmenn@thoughtworks.com>
2016-07-22 15:49:19 +0530

    fixed switch_structure_test.go tests

--- control_structures/switch_structure.go
+++ control_structures/switch_structure.go
@@ -6,5 +6,15 @@ const multiply string = "*"
 const divide = "/"
 
 func calculate(op string, arg1, arg2 float32) float32 {
-	return 0.0
+	switch {
+	case op == add:
+		return arg1 + arg2
+	case op == subtract:
+		return arg1 - arg2
+	case op == multiply:
+		return arg1 * arg2
+	case op == divide:
+		return arg1 / arg2
+	}
+	return 0
 }
