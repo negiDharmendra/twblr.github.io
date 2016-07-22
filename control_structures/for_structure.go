435ac231cb733d09e2ef8d749771745e35ab6c5f
Dharmendra Singh <dharmenn@thoughtworks.com>
2016-07-22 16:06:33 +0530

    fixed for_structure_test.go tests

--- control_structures/for_structure.go
+++ control_structures/for_structure.go
@@ -1,5 +1,15 @@
 package control_structures
 
 func collatzChainLength(n int) int {
-	return 0
+	i := 0
+	for {
+		if n == 1 {
+			return i
+		} else if n%2 == 0 {
+			n = n / 2
+		} else {
+			n = (3 * n) + 1
+		}
+		i++
+	}
 }
