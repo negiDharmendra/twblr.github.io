c339d064b7e9f0916acbf917701c320c9de157f7
Dharmendra Singh <dharmenn@thoughtworks.com>
2016-07-22 15:28:28 +0530

    fixed if_structure_test.go tests

--- control_structures/if_structure.go
+++ control_structures/if_structure.go
@@ -1,5 +1,16 @@
 package control_structures
 
+import (
+	"strconv"
+)
+
 func fizzBuzz(i int32) string {
-	return ""
+	if i%3 == 0 && i%5 == 0 {
+		return "FizzBuzz"
+	} else if i%3 == 0 {
+		return "Fizz"
+	} else if i%5 == 0 {
+		return "Buzz"
+	}
+	return strconv.Itoa(int(i))
 }
