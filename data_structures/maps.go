91f1e837f607a8b7ecafe5225dec1bbf03afef96
Dharmendra Singh <dharmenn@thoughtworks.com>
2016-07-22 17:53:30 +0530

    added findPeopleWithCommonInterest and  contains method to fix tests in maps_test.go

--- data_structures/maps.go
+++ data_structures/maps.go
@@ -1,9 +1,21 @@
 package data_structures
 
+
 func findPeopleWithCommonInterest(data map[string][]string, interest string) []string {
-	return nil
+	result := []string{}
+	for key,value := range data {
+		if contains(value,interest){
+			result  = append(result,key)
+		}
+	}
+	return result
 }
 
 func contains(src []string, elem string) bool {
+	for i := range src{
+		if src[i] == elem{
+			return true
+		}
+	}
 	return false
 }
