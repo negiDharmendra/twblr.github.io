53da6bf5eadba0d39417af9a9647c2e38ab65ef1
Dharmendra Singh <dharmenn@thoughtworks.com>
2016-07-23 10:30:27 +0530

    fixed shape problems

--- interfaces/shape.go
+++ interfaces/shape.go
@@ -3,3 +3,28 @@ package interfaces
 type Shape interface {
 	Area() int
 }
+
+type Square struct {
+	side int
+}
+
+type Rectangle struct {
+	length int
+	breadth int
+}
+
+type Hybrid struct {
+	square Square
+	rectangle Rectangle
+}
+
+func (this *Square) Area() int{
+	return this.side * this.side
+}
+func (this *Rectangle) Area() int{
+	return this.length * this.breadth
+}
+
+func (this *Hybrid) Area() int{
+	return this.square.Area() + this.rectangle.Area()
+}
